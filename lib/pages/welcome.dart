e82f2da730935ec7cef008634da95f71501e6ed3
Juan J Tovar <54806953+JuanJTovar@users.noreply.github.com>
2019-11-25 08:19:37 -0600

    Add files via upload

---
+++ lib/pages/welcome.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+import 'dashboard.dart';
+
+class Welcome extends StatefulWidget {
+  @override
+  _WelcomeState createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.fromLTRB(25, 100, 25, 25),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Image.asset(
+                  'assets/img/illustration.png',
+                  width: 300,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 50),
+                ),
+                Text(
+                  'Personal',
+                  style: TextStyle(
+                    color: Theme.of(context).accentColor,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'Entrenador'.toUpperCase(),
+                  style: TextStyle(
+                    fontSize: 48,
+                    color: Theme.of(context).primaryColor,
+                    fontFamily: 'Bebas',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 30),
+                ),
+                Text(
+                  'Esta aplicacion "Entrenador" es para los alumnos del grupo 15701 de la clase del profesor Leonardo, los demas no pueden usarla.',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 30),
+                ),
+                MaterialButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Dashboard(),
+                      ),
+                    );
+                  },
+                  minWidth: double.infinity,
+                  height: 50,
+                  child: Text(
+                    'Empezemos'.toUpperCase(),
+                  ),
+                  color: Theme.of(context).primaryColor,
+                  textColor: Colors.lightGreenAccent,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10),
+                ),
+                MaterialButton(
+                  onPressed: () {},
+                  minWidth: double.infinity,
+                  height: 50,
+                  textColor: Theme.of(context).primaryColor,
+                  child: Text(
+                    'Ingresar'.toUpperCase(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
